c0608f30ad28c71ce7bd915feb16f6c30ab8714a
Junian Triajianto <author@junian.dev>
2026-09-12 11:09:01 +0700

    Refactor version handling for Microsoft Remote Desktop cask to improve compatibility with macOS Catalina

--- Casks/microsoft-remote-desktop-for-mac.rb
+++ Casks/microsoft-remote-desktop-for-mac.rb
@@ -1,27 +1,27 @@
 cask "microsoft-remote-desktop-for-mac" do
   on_big_sur :or_older do
     # macOS 10.12 (Sierra)
-    on_sierra :or_older do
-      version "10.3.9"
-      sha256 "40970f99d9ecebb585173531fb1411bad1b65d5a228ed7718a4f435976210d6f"
-    end
+    # on_sierra :or_older do
+    #   version "10.3.9"
+    #   sha256 "40970f99d9ecebb585173531fb1411bad1b65d5a228ed7718a4f435976210d6f"
+    # end
     # macOS 10.13 (High Sierra)
-    on_high_sierra do
-      version "10.5.2"
-      sha256 "b648e704f3701116b9abbae7d5960a87fd45d85e9e129561c3cbfd692e48bb96"
-    end
+    # on_high_sierra do
+    #   version "10.5.2"
+    #   sha256 "b648e704f3701116b9abbae7d5960a87fd45d85e9e129561c3cbfd692e48bb96"
+    # end
     # macOS 10.14 (Mojave)
-    on_mojave do
-      version "10.8.4"
-      sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
-    end
+    # on_mojave do
+    #   version "10.8.4"
+    #   sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
+    # end
     # macOS 10.15 (Catalina)
-    on_catalina do
+    if MacOS.version == :catalina
       version "10.8.4"
       sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
     end
     # macOS 11 (Big Sur)
-    on_big_sur do
+    on_big_sur :or_older do
       version "10.9.5"
       sha256 "c248d61cd6ebb70dcec9a58c0451806ca3c920b6452498cb37317716ae997457"
     end
