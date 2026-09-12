218a7eaa1c63645f31897083020e19fbf912f73c
Junian Triajianto <author@junian.dev>
2026-09-12 11:11:36 +0700

    Refactor version handling for Microsoft Remote Desktop cask to improve clarity and maintainability

--- Casks/microsoft-remote-desktop-for-mac.rb
+++ Casks/microsoft-remote-desktop-for-mac.rb
@@ -1,27 +1,27 @@
 cask "microsoft-remote-desktop-for-mac" do
-  on_big_sur :or_older do
+  if MacOS.version <= :big_sur
     # macOS 10.12 (Sierra)
-    # on_sierra :or_older do
-    #   version "10.3.9"
-    #   sha256 "40970f99d9ecebb585173531fb1411bad1b65d5a228ed7718a4f435976210d6f"
-    # end
+    if MacOS.version <= :sierra
+      version "10.3.9"
+      sha256 "40970f99d9ecebb585173531fb1411bad1b65d5a228ed7718a4f435976210d6f"
+    end
     # macOS 10.13 (High Sierra)
-    # on_high_sierra do
-    #   version "10.5.2"
-    #   sha256 "b648e704f3701116b9abbae7d5960a87fd45d85e9e129561c3cbfd692e48bb96"
-    # end
+    if MacOS.version == :high_sierra
+      version "10.5.2"
+      sha256 "b648e704f3701116b9abbae7d5960a87fd45d85e9e129561c3cbfd692e48bb96"
+    end
     # macOS 10.14 (Mojave)
-    # on_mojave do
-    #   version "10.8.4"
-    #   sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
-    # end
+    if MacOS.version == :mojave
+      version "10.8.4"
+      sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
+    end
     # macOS 10.15 (Catalina)
     if MacOS.version == :catalina
       version "10.8.4"
       sha256 "158e6d5e0ecbf2421463c95e754fd9e422f93dd5ddcceccb50ecb5a0b24d9ac9"
     end
     # macOS 11 (Big Sur)
-    on_big_sur :or_older do
+    if MacOS.version >= :big_sur
       version "10.9.5"
       sha256 "c248d61cd6ebb70dcec9a58c0451806ca3c920b6452498cb37317716ae997457"
     end
@@ -30,8 +30,9 @@ cask "microsoft-remote-desktop-for-mac" do
       skip "Legacy version"
     end
   end
+
   # macOS 12 (Monterey)
-  on_monterey :or_newer do
+  if MacOS.version >= :monterey
     version "10.9.10"
     sha256 "732cf7f5f74f67032455907cb29743e53b6733770e0a92ebc6243e75e5fd99af"
 
